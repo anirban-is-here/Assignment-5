1c70fbcd24279339b678c907361f7fff64ade92c
Anirban <anirbanmukherjee.cse@gmail.com>
2025-08-28 21:09:57 +0600

    all functionalities covered

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const callHistory = document.getElementById("call-history");
 const clearBtn = document.getElementById("clear-history")
 let heartCount = 0;
 
+
+
 // function for setting attribute
 function setInnerText(element, value) {
   if (element) {
@@ -24,6 +26,7 @@ for (const icon of heartIcons) {
 // call button functionality
 
 const callBtns = document.querySelectorAll(".call");
+
 
 for (const btn of callBtns) {
     btn.addEventListener('click', function(){
@@ -71,3 +74,37 @@ for (const btn of callBtns) {
 clearBtn.addEventListener("click", function(){
     callHistory.innerHTML = "";
 });
+
+// Copy button functionality
+const copyBtns = document.querySelectorAll(".copy");
+const topCopyBtn = document.getElementById("copy-count");
+let topCopyBtnVal = parseInt(topCopyBtn.innerText);
+let count = topCopyBtnVal;
+for (const copyBt of copyBtns) {
+    copyBt.addEventListener('click', function(){
+        count += 1;
+
+        setInnerText(topCopyBtn, count);
+        alert("Number Copppied")
+    
+  });
+  
+}
+
+// Copy functionality
+
+
+
+for (const copyBt of copyBtns) {
+  copyBt.addEventListener('click', function() {
+    const number = copyBt.getAttribute("data-num");
+    navigator.clipboard.writeText(number)
+      .then(() => {
+        
+        alert(`Copied: ${number}`);
+      })
+      .catch(() => {
+        alert('Failed to copy!');
+      });
+  });
+}
